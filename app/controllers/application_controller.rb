0d3ff04686cf5ca3135fa0e8689e699f7eb9029e
axelrod <fasolshik@yahoo.com>
2022-12-17 23:27:14 +0600

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_user_can_edit?
-
+  test
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
